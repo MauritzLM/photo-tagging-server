86d787f76e62968e2eea67a0c100ae4774ef5667
Mauritz Marais <mauritz303@gmail.com>
2023-10-18 16:44:15 +0200

    added node-postgress package

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
+require('dotenv').config();
 const express = require('express');
+
+
+const app = express();
+
+const port = process.env.PORT || 3004;
+
+app.get('/test', (req,res) => {
+    res.send('Hello')
+})
+
+app.listen(port, () => {
+    console.log(`listening on port ${port}`)
+});
+
+// set up cors, json, middleware*
