f58396c3e0f4021b00db10e691608538ec1e5fa3
Mauritz Marais <mauritz303@gmail.com>
2023-10-19 13:36:44 +0200

    added routes

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 require('dotenv').config();
 const express = require('express');
+const db = require('./db');
+const routes = require('./routes/index');
 
 
 const app = express();
 
 const port = process.env.PORT || 3004;
 
-app.get('/test', (req,res) => {
-    res.send('Hello')
-})
+// routes
+app.use('/', routes);
+
 
 app.listen(port, () => {
     console.log(`listening on port ${port}`)
