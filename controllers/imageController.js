bfaf942819e3ca9a5acbcd345feeacf3f5a7c88e
Mauritz Marais <mauritz303@gmail.com>
2023-10-26 17:06:03 +0200

    updated image details function to query db given req body and send true or false response

--- controllers/imageController.js
+++ controllers/imageController.js
@@ -1,20 +1,43 @@
 const db = require('../db');
+const helpers = require('../helpers/helpers');
 
 // get image info
 exports.getImageDetails = async function (req, res, next) {
+    
     try {
 
-        // sample query
-        const result = await db.query("SELECT score FROM test WHERE username IN ('Mo')");
+        // get values from req(x, y, character)
+        const { x, y, image, character } = req.body;
 
-        console.log(result);
+        // console.log(req.body);
 
-        res.status(200).json(result);
-        // req comes with numbers from click and image id
-        // query db to get numbers of image
-        // compare numbers
+        // create parameterized query
+        const text = "SELECT * FROM $1 WHERE name = $2";
+        const values = [image, character];
+
+        // query db
+        const result = await db.query(text, values);
+
+        // save row returned
+        const row = result.rows[0];
+
+        // create character values object
+        const characterValues = {
+            x_min: row["x_min"],
+            x_max: row["x_max"],
+            y_min: row["y_min"],
+            y_max: row["y_max"]
+        }
+
+        // check if tag is correct
+        const isCorrect = helpers.compareValues(x, y, characterValues);
+
+
         // send result
-    } catch (error) {
+        res.status(200).json(isCorrect);
+
+    }
+    catch (error) {
         return next(error);
     }
 };
