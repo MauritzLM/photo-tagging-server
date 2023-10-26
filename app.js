48e62856b04736e75660b3ec87081f560599cf6d
Mauritz Marais <mauritz303@gmail.com>
2023-10-26 17:04:35 +0200

    installed cors

--- app.js
+++ app.js
@@ -2,10 +2,17 @@ require('dotenv').config();
 const express = require('express');
 const db = require('./db');
 const routes = require('./routes/index');
+const cors = require('cors');
+
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    optionsSuccessStatus: 200
+};
 
 const app = express();
 
 app.use(express.json());
+app.use(cors(corsOptions));
 
 // routes
 app.use('/', routes);
