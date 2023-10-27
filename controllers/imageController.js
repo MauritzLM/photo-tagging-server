e9947b6e917d130bb4001fb42d1f97d7a0f3c210
Mauritz Marais <mauritz303@gmail.com>
2023-10-27 16:24:43 +0200

    parameterized db query

--- controllers/imageController.js
+++ controllers/imageController.js
@@ -5,15 +5,12 @@ const helpers = require('../helpers/helpers');
 exports.getImageDetails = async function (req, res, next) {
     
     try {
-
-        // get values from req(x, y, character)
+        // get values from request
         const { x, y, image, character } = req.body;
 
-        // console.log(req.body);
-
         // create parameterized query
-        const text = "SELECT * FROM $1 WHERE name = $2";
-        const values = [image, character];
+        const text = 'SELECT * FROM characters WHERE name = $1 AND image = $2';
+        const values = [character, image];
 
         // query db
         const result = await db.query(text, values);
@@ -27,11 +24,10 @@ exports.getImageDetails = async function (req, res, next) {
             x_max: row["x_max"],
             y_min: row["y_min"],
             y_max: row["y_max"]
-        }
+        };
 
         // check if tag is correct
         const isCorrect = helpers.compareValues(x, y, characterValues);
-
 
         // send result
         res.status(200).json(isCorrect);
