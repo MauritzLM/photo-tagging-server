491172e55c830045ba264e2d9d8cae605ab814d5
Mauritz Marais <mauritz303@gmail.com>
2023-11-01 16:23:06 +0200

    updated function updateLeaderboard to record player name of game instance

--- controllers/leaderboardController.js
+++ controllers/leaderboardController.js
@@ -1,29 +1,45 @@
 const db = require('../db');
 
 // get leaderboard
-exports.getLeaderboard = async function(req,res,next) {
-      try {
+exports.getLeaderboard = async function (req, res, next) {
+   try {
 
-        const result = await db.query('SELECT username FROM test')
-         // query db to get leaderboard
-         // sort leaderboard by score
-         // send back json  
-         res.status(200).json(result);
-      } 
-      catch (error) {
-        return next(error);
-      }
+      // clear table? (where end time = null)*
+
+      const { image } = req.body;
+
+      const text = 'SELECT * FROM game_instance WHERE image = $1';
+      const values = [image];
+
+      // query db to get leaderboard
+      const result = await db.query(text, values);
+
+      // sort by score*
+
+      // send back json  
+      res.status(200).json(result);
+   }
+   catch (error) {
+      return next(error);
+   }
 }
 
-// update leaderboard
-exports.updateLeaderboard = async function(req,res,next) {
-    try {
-       // req comes with form data
-       // check table and compare scores?
-       // create new row in table with form data
-       //     
-    }
-     catch (error) {
-        return next(error);
-    }
+// add name to game instance*
+exports.updateLeaderboard = async function (req, res, next) {
+   try {
+
+      // validate and sanitize data*
+
+      const { id, name } = req.body;
+
+      const text = 'INSERT INTO game_instance (player_name) VALUES($1) WHERE id = $2'
+      const values = [name, id];
+
+      const result = await db.query(text, values);
+
+      res.json(result);
+   }
+   catch (error) {
+      return next(error)
+   }
 }
