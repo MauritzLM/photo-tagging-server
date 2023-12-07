4305095458815642859d45e9e153c8f6beb540cf
Mauritz Marais <mauritz303@gmail.com>
2023-12-07 12:31:22 +0200

    added env variable

--- db/index.js
+++ db/index.js
@@ -2,7 +2,7 @@ const { Pool, Client } = require('pg');
 
 
 const pool = new Pool({
-    host: process.env.DATABASE_URL,
+    host: process.env.DATABASE_PRIVATE_URL,
     user: process.env.PGUSER,
     password: process.env.PGPASSWORD,
     port: process.env.PGPORT,
