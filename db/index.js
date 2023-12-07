fd65d385af8ac41e7bf38e14c1cfeafa19244d63
Mauritz Marais <mauritz303@gmail.com>
2023-12-07 12:45:44 +0200

    updated db connection

--- db/index.js
+++ db/index.js
@@ -1,15 +1,18 @@
 const { Pool, Client } = require('pg');
 
 
-const pool = new Pool({
-    host: process.env.DATABASE_PRIVATE_URL,
-    user: process.env.PGUSER,
-    password: process.env.PGPASSWORD,
-    port: process.env.PGPORT,
-    database: process.env.PGDATABASE
-})
+const pool = new Pool();
 
 
 module.exports = {
     query: (text, params) => pool.query(text, params)
 }
+
+
+// const pool = new Pool({
+//     host: process.env.DATABASE_PRIVATE_URL,
+//     user: process.env.PGUSER,
+//     password: process.env.PGPASSWORD,
+//     port: process.env.PGPORT,
+//     database: process.env.PGDATABASE
+// })
